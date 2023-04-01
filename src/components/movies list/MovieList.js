a10b3aaf281515e766bce71fb663af00a9b6b166
Stefan Cepeleaga <108405253+StefanC24@users.noreply.github.com>
2023-04-01 19:39:06 +0000

    added pagination function

--- src/components/movies list/MovieList.js
+++ src/components/movies list/MovieList.js
@@ -5,19 +5,39 @@ import './MovieList.css'
 function MovieList(){
 
     const [movie, setMovie] = React.useState()
-
+    const [currentPage, setCurrentPage] = React.useState(1)
+  
     React.useEffect(()=>
-        fetchMovies()
+        fetchMovies(1)
     , [])
 
-    const fetchMovies = () => {
+    
+    const fetchMovies = (currentPageNumber) => {
         axios.get('https://api.themoviedb.org/3/movie/popular?api_key=dc0fb7b28045cd04916b73e857aec4f9')
-        .then(response => setMovie(response.data.results))
+        .then(response => 
+            setMovie(response.data.results))
+            setCurrentPage(currentPageNumber)
     }
+    
+    const changePage = (currentPageNumber, direction) => {
+       if(currentPageNumber === 1 && direction === 'previous'){
+        return
+       }
+
+       if(direction === 'next'){
+        fetchMovies(currentPageNumber + 1)
+       }
+       else if(direction === 'previous'){
+        fetchMovies(currentPageNumber - 1)
+       }
+    }
+
 
     return (
         <div className='movies-container'>
             <h1>Most popular</h1>
+            <button onClick={() => changePage(currentPage, 'previous')}>Previous</button>
+            <button onClick={() =>  changePage(currentPage, 'next')}>Next</button>
             <div className='cards-container'>
                 {movie?.map(item => <MovieCard {...item}/>)}
             </div>
