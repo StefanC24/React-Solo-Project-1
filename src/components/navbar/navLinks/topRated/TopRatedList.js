8440677ecc0c46c8952c05951cb798b96cb234b6
John DoeStefan <stefancepeleaga@gmail.com>
2023-04-09 00:13:56 +0100

    added next and prev button to top rated link

--- src/components/navbar/navLinks/topRated/TopRatedList.js
+++ src/components/navbar/navLinks/topRated/TopRatedList.js
@@ -5,29 +5,46 @@ import TopRatedCard from "./TopRatedCard";
 function TopRatedList(){
 
     const [movieCard, setMovieCard] = useState();
+    const [currentPage, setCurrentPage] = useState(1)
 
-    const fetchMovieCard = () => {
-        axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=dc0fb7b28045cd04916b73e857aec4f9')
-        .then(response => setMovieCard(response.data.results))
+    useEffect(() => {
+        fetchMovieCard(1)
+    },[])
+
+    const handleClick = (currentPageNumber, direction) => {
+        if(currentPageNumber === 1 && direction === 'previous'){
+            return
+        }
+        if(direction === 'previous'){
+           fetchMovieCard(currentPageNumber - 1);
+        }
+        else if(direction === 'next'){
+            fetchMovieCard(currentPageNumber + 1);
+        }
+    }
+
+    const fetchMovieCard = (currentPageNumber) => {
+        axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=dc0fb7b28045cd04916b73e857aec4f9&page=${currentPageNumber}`)
+        .then(response => 
+            setMovieCard(response.data.results))
+            setCurrentPage(currentPageNumber)
+
     }
     
-    useEffect(() => {
-        fetchMovieCard()
-    })
 
     return(
         <div className="movies-container">
             <h1 className='header'>Top Rated Movies</h1>
             <div className='buttons-container'>
-                <button className='previous-button button' >Previous</button>
-                <button className='next-button button'>Next</button>
+                <button className='previous-button button' onClick={() => handleClick(currentPage, 'previous')} >Previous</button>
+                <button className='next-button button' onClick={() => handleClick(currentPage, 'next')}>Next</button>
             </div>  
             <div className="cards-container">
                 {movieCard?.map(items => <TopRatedCard {...items}/>)}
             </div>
             <div className='buttons-container'>
-                <button className='previous-button button' >Previous</button>
-                <button className='next-button button'>Next</button>
+                <button className='previous-button button' onClick={() => handleClick(currentPage, 'previous')} >Previous</button>
+                <button className='next-button button' onClick={() => handleClick(currentPage, 'next')}>Next</button>
             </div>  
         </div>
     )
