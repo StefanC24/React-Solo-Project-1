29f92575e4c9b62b9b4fd3665aaf9fcb55e35244
StefanC24 <stefancepeleaga@gmail.com>
2023-04-09 10:56:29 +0100

    added logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,9 @@ import './Navbar.css'
 function Navbar(){
     return(
         <div className="navbar">
+            <Link to={'/'}>
+                <img className='logo' src='logos/MovieCatalogue-logos_black.png' alt=''></img>
+            </Link>
             <ul className="navbar-links">
                 <li><Link className='nav-item' to='/'>Home</Link></li>
                 <li><Link className='nav-item' to='/top-rated-movies'>Top Rated Movies</Link></li>
