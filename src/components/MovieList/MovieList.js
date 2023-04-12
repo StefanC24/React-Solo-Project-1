4fa60a289d769d3b63786aeebf9a29a1d3b3b337
StefanC24 <stefancepeleaga@gmail.com>
2023-04-12 16:47:26 +0100

    added search functionality

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -34,13 +34,17 @@ function MovieList(){
     return (
         <div className='movies-container'>
             <h1 className='header'>Most popular</h1>
-            <input onChange={(event)=>setInput(event.target.value)} type='text'placeholder='Search'></input>
+            <input className='search' onChange={(event)=>setInput(event.target.value)} type='text'placeholder='Search'></input>
             <div className='buttons-container'>
                 <button className='previous-button button' onClick={() => changePage(currentPage, 'previous')}>Previous</button>
                 <button className='next-button button' onClick={() =>  changePage(currentPage, 'next')}>Next</button>
             </div>  
             <div className='cards-container'>
-                {movies?.map(item => <MovieCard {...item}/>)}
+                {movies?.filter((item)=>{
+                    return input.toLowerCase() === "" 
+                    ? 
+                    item : item.title.toLowerCase().includes(input)
+                }).map(item => <MovieCard {...item} />)}
             </div>
             <div className='buttons-container'>
                 <button className='previous-button button' onClick={() => changePage(currentPage, 'previous')}>Previous</button>
